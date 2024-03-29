2410642c77854d25d012e1bd692b1c526635cc02
luis486 <luis.murcia1@u.icesi.edu.co>
2024-03-29 17:55:48 -0500

    Done!
    ecommerce-admin up

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="bwjp1qjl">
         {({ open }) => {
           const onClick = () => {
             open();
